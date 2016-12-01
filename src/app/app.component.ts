b1adbafae49392650057cc22aed45a3e1ab80434
P1xt <kjphpdev@gmail.com>
2016-11-30 19:25:47 -0600

    app updated up through routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { HomeComponent } from './home/home.component';
+import { RouterOutlet } from "@angular/router";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  directives: [HomeComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'app works!';
+  name = 'ninja';
+  ninja = {
+    name: "Ryu",
+    belt: "Red"
+  };
+  yell(e) {
+    alert("I am yelling");
+  }
 }
